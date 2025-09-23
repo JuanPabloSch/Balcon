2b4b098610ddf357df6c68eab46adbab2bfd2e7f
JuanPabloSch <juanpablosch@gmail.com>
2025-09-23 13:11:37 -0300

    Versión inicial del sitio

---
+++ js/include.js
@@ -0,0 +1,16 @@
+function includeHTML(id, file) {
+    fetch(file)
+    .then(response => {
+        if (!response.ok) throw new Error("Error al cargar " + file);
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById(id).innerHTML = data;
+    })
+    .catch(error => console.error(error));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (document.getElementById("navbar")) includeHTML("navbar", "navbar.html");
+    if (document.getElementById("footer")) includeHTML("footer", "footer.html");
+});
